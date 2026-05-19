c0caf293b354583f1d38f183a9c9503ea096f612
zy <761478861@qq.com>
2026-05-19 16:03:23 +0800

    first commit

---
+++ src/main/java/com/yupi/yupictruebackend/YuPictrueBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.yupictruebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuPictrueBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuPictrueBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yupi/yupictruebackend/YuPictrueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.yupictruebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuPictrueBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
